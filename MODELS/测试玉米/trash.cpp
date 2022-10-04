fc10faf55f08753c1cee06cec28c7abf5918d2ad
Sakkana <98660197+Sakkana@users.noreply.github.com>
2022-10-04 20:18:17 +0800

    Delete MODELS directory

--- MODELS/测试玉米/trash.cpp
+++
@@ -1,9 +0,0 @@
-get boundary of x, y, z axis
-    (x_min, y_min, z_min)
-    (x_max, y_max, z_max)
-
-normalization
-    -1 <= x, y, z <= 1
-
-map
-    (x, y, z) -> ((π + atan2(z, x)) / (2 * π), (1 + y) / 2, 1)
